b0ed91031fc6ec515572acbf550c23b5bdee7c57
TigerSis <1549776544@qq.com>
2020-08-05 21:12:46 +0800

    move1.1 解决speed取整归零，并且将if else写成三元运算符

--- move.js
+++ move.js
@@ -1,13 +1,21 @@
-// move运动框架1.0
-function move(obj, name, target) {
-	clearInterval(obj.timer)
-	obj.timer = setInterval(function() {
-		let nowStyle = parseInt(getComputedStyle(obj)[name])
-		let speed = (target - nowStyle) / 10
-		if (nowStyle == target) {
-			clearInterval(obj.timer)
-		} else {
-			obj.style[name] = nowStyle + speed + 'px'
-		}
-	}, 30)
-}
+// 运动框架1.1
+	function move(obj, name, target) {
+		clearInterval(obj.timer)
+		obj.timer = setInterval(function() {
+			let nowStyle=parseInt(getComputedStyle(obj)[name])
+				let speed = (target - nowStyle) / 10
+				
+				// if(speed>0) {
+				// 	speed=Math.ceil(speed)
+				// } else {
+				// 	speed=Math.floor(speed)
+				// }
+				speed=(speed>0)?Math.ceil(speed):speed=Math.floor(speed)
+				
+			if (nowStyle == target) {
+				clearInterval(obj.timer)
+			} else {
+				obj.style[name] = nowStyle + speed + 'px'
+			}
+		}, 30)
+	}
