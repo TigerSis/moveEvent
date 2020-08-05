09aeb7b70eb3d39f6eff239f553941a205bc6d79
TigerSis <1549776544@qq.com>
2020-08-05 22:59:07 +0800

    move1.4 新增兼容没有单位的属性名，例如背景色

--- move.js
+++ move.js
@@ -1,27 +1,29 @@
 // 运动框架1.3
-	function move(obj, name, target,fn) {
-		clearInterval(obj.timer)
-		obj.timer = setInterval(function() {
-			if(name=='opacity') {
-				nowStyle=getComputedStyle(obj)[name]*100
-			}else {
-			    nowStyle=parseInt(getComputedStyle(obj)[name])
+function move(obj, name, target, fn) {
+	clearInterval(obj.timer)
+	obj.timer = setInterval(function() {
+			if (name == 'opacity') {
+				nowStyle = getComputedStyle(obj)[name] * 100
+			} else if (name == 'width' || name == 'height' || name == 'border-radius' || name == 'border-width' || name =='font-size') {
+				nowStyle = parseInt(getComputedStyle(obj)[name])
+			} else {
+				nowStyle = getComputedStyle(obj)[name]
 			}
-			
+
 			let speed = (target - nowStyle) / 10
-				
-			speed=(speed>0)?Math.ceil(speed):speed=Math.floor(speed)
-				
+			speed = (speed > 0) ? Math.ceil(speed):Math.floor(speed)
+
 			if (nowStyle == target) {
 				clearInterval(obj.timer)
 				if (fn) {fn()}
 			} else {
-				if (name=='opacity') {
-				obj.style[name] = (nowStyle + speed)/100
-				} else{
+				if (name == 'opacity') {
+				obj.style[name] = (nowStyle + speed) / 100
+			} else  if (name == 'width' || name == 'height' || name == 'border-radius' || name == 'border-width' || name =='font-size') {
 				obj.style[name] = nowStyle + speed + 'px'
-				}
-				
+			} else {
+				obj.style[name] = target
 			}
-		}, 30)
-	}
+		}
+	}, 30)
+}
