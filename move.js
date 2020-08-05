421222f8d0a5aa4e130e9558a945a8d023bdf3b6
TigerSis <1549776544@qq.com>
2020-08-05 21:16:13 +0800

    move1.2 链式运动：有先后的运动

--- move.js
+++ move.js
@@ -1,19 +1,17 @@
-// 运动框架1.1
-	function move(obj, name, target) {
+// 运动框架1.2
+// 链式运动
+	function move(obj, name, target,fn) {
 		clearInterval(obj.timer)
 		obj.timer = setInterval(function() {
 			let nowStyle=parseInt(getComputedStyle(obj)[name])
-				let speed = (target - nowStyle) / 10
+			let speed = (target - nowStyle) / 10
 				
-				// if(speed>0) {
-				// 	speed=Math.ceil(speed)
-				// } else {
-				// 	speed=Math.floor(speed)
-				// }
 				speed=(speed>0)?Math.ceil(speed):speed=Math.floor(speed)
 				
 			if (nowStyle == target) {
 				clearInterval(obj.timer)
+				// 判断fn是否存在,存在即执行
+				if (fn) {fn()}
 			} else {
 				obj.style[name] = nowStyle + speed + 'px'
 			}
