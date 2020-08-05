84f601c53e7bc8348548f124e819a1cc8af33a70
TigerSis <1549776544@qq.com>
2020-08-05 21:36:39 +0800

    move1.3 兼容透明度

--- move.js
+++ move.js
@@ -1,19 +1,27 @@
-// 运动框架1.2
-// 链式运动
+// 运动框架1.3
 	function move(obj, name, target,fn) {
 		clearInterval(obj.timer)
 		obj.timer = setInterval(function() {
-			let nowStyle=parseInt(getComputedStyle(obj)[name])
+			if(name=='opacity') {
+				nowStyle=getComputedStyle(obj)[name]*100
+			}else {
+			    nowStyle=parseInt(getComputedStyle(obj)[name])
+			}
+			
 			let speed = (target - nowStyle) / 10
 				
-				speed=(speed>0)?Math.ceil(speed):speed=Math.floor(speed)
+			speed=(speed>0)?Math.ceil(speed):speed=Math.floor(speed)
 				
 			if (nowStyle == target) {
 				clearInterval(obj.timer)
-				// 判断fn是否存在,存在即执行
 				if (fn) {fn()}
 			} else {
+				if (name=='opacity') {
+				obj.style[name] = (nowStyle + speed)/100
+				} else{
 				obj.style[name] = nowStyle + speed + 'px'
+				}
+				
 			}
 		}, 30)
 	}
